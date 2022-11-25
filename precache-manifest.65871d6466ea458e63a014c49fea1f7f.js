d4088af459fc727325ca3cb3d9996293aa922e13
Code Sandbox <hello@codesandbox.io>
2022-11-25 11:42:19 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.65871d6466ea458e63a014c49fea1f7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d8355192504c200b2939",
+    "url": "https://samging.github.io/csb-8sw9qm/static/css/main.2aaf4a46.chunk.css"
+  },
+  {
+    "revision": "d8355192504c200b2939",
+    "url": "https://samging.github.io/csb-8sw9qm/static/js/main.d8355192.chunk.js"
+  },
+  {
+    "revision": "d01e1c9664616aab8d16",
+    "url": "https://samging.github.io/csb-8sw9qm/static/js/1.d01e1c96.chunk.js"
+  },
+  {
+    "revision": "739fd5f2d32e4397eb32",
+    "url": "https://samging.github.io/csb-8sw9qm/static/js/runtime~main.739fd5f2.js"
+  },
+  {
+    "revision": "654bd0eedfdd557de72454cc0a0e42b6",
+    "url": "https://samging.github.io/csb-8sw9qm/static/media/logo.654bd0ee.svg"
+  },
+  {
+    "revision": "49a513444ae5277fcc3bfe2b5f2551d9",
+    "url": "https://samging.github.io/csb-8sw9qm/index.html"
+  }
+];
